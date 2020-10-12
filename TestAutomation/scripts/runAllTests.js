8684c94e9e6efb03acea2b8e9831c0ae21948c5c
hafey1 <niggebruggere@g.cofc.edu>
2020-10-12 04:52:48 -0400

    script almost working

--- TestAutomation/scripts/runAllTests.js
+++ TestAutomation/scripts/runAllTests.js
@@ -1,5 +1,45 @@
 const fsLibrary = require('fs');
-let data = 'hello there';
+
+function resolveAfter2Seconds() {
+    return new Promise( resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+
+        }, 2000);
+    });
+}
+
+
+
+
+
+async function asyncReadWrite() {
+let incomingInfo = []
+fsLibrary.readdir('../testCases/', function(err, fileNames) {
+    if (err) {
+        throw err;
+        }
+       
+    fileNames.forEach(function(filename) {
+        fsLibrary.readFile('../testCases/' + filename, "utf-8", function(err, content) {
+            if (err) {
+                throw err;
+                
+            }
+
+            incomingInfo.push(JSON.parse(content))
+        });
+    });
+});
+ console.log("processing Test Cases.....")
+const waiting = await resolveAfter2Seconds();
+console.log('Finished Processing ' + incomingInfo.length + " Test Case(s).")
+let data = 'hello there' + incomingInfo[1].module;
 fsLibrary.writeFile('../project/MarsMapMaker-1/src/__tests__/testRunner.js', data, (error) => {
     if (error) throw error;
-    })
+    }) 
+
+}
+
+
+asyncReadWrite();
