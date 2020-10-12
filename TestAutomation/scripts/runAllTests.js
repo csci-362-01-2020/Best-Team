f9e06e032921e0a911571a6ac1f6c13fe38e3dda
hafey1 <niggebruggere@g.cofc.edu>
2020-10-12 14:12:51 -0400

    test Runner has imports

--- TestAutomation/scripts/runAllTests.js
+++ TestAutomation/scripts/runAllTests.js
@@ -1,5 +1,6 @@
 const fsLibrary = require('fs');
 
+//this allows for the testcases to be read before outputting to testRunner
 function resolveAfter2Seconds() {
     return new Promise( resolve => {
         setTimeout(() => {
@@ -9,12 +10,25 @@ function resolveAfter2Seconds() {
     });
 }
 
+function testRunnerContent(fromCases) {
+    let imports = "import React from \'react\';\nimport { shallow } from \'enzyme\'\n//{ } imports non default component\n"
+    console.log("sdfasffdsfa " + fromCases.length)
+    for (let i = 0; i < fromCases.length; i++) {
+        let tCase = fromCases[i]
+        if (!imports.includes(tCase.module))
+            imports = imports + "import { " + tCase.module + " } from \"../components/" + tCase.module + ";\n"
+    }
+    imports = imports + "\n"
+
+    return imports;
+}
 
 
 
 
+//Reads testcases and writes to test runner based on results
 async function asyncReadWrite() {
-let incomingInfo = []
+let infoForTestRunner = []
 fsLibrary.readdir('../testCases/', function(err, fileNames) {
     if (err) {
         throw err;
@@ -27,19 +41,27 @@ fsLibrary.readdir('../testCases/', function(err, fileNames) {
                 
             }
 
-            incomingInfo.push(JSON.parse(content))
+            infoForTestRunner.push(JSON.parse(content))
         });
     });
 });
  console.log("processing Test Cases.....")
+
 const waiting = await resolveAfter2Seconds();
-console.log('Finished Processing ' + incomingInfo.length + " Test Case(s).")
-let data = 'hello there' + incomingInfo[1].module;
+
+console.log('Finished Processing ' + infoForTestRunner.length + " Test Case(s).")
+
+//this will write the proper file information soon
+const data = testRunnerContent(infoForTestRunner);
+
+//write the info to testRunner
 fsLibrary.writeFile('../project/MarsMapMaker-1/src/__tests__/testRunner.js', data, (error) => {
+    
     if (error) throw error;
+    
     }) 
 
 }
 
-
+// call the function
 asyncReadWrite();
--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,1 +1,6 @@
 node runAllTests.js
+echo "building tests.."
+sleep 2
+echo "done"
+cd ../project/MarsMapMaker-1/src/__tests__
+node testRunner.js
