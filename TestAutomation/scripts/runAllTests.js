ebc1c1f5ebe15b832936fd2613a9d0d68cfa8d65
hafey1 <niggebruggere@g.cofc.edu>
2020-10-12 03:16:22 -0400

    messing with scripts

--- TestAutomation/scripts/runAllTests.js
+++ TestAutomation/scripts/runAllTests.js
@@ -1,9 +1,5 @@
-<script>
-    const fsLibrary = require('fs')
-
-    let data = 'hello there'
-    
-    fsLibrary.writeFile('../projects/MarsMapMaker-1/src/__tests__/testRunner.js', data, (error) => {
-        if (error) throw err;
+const fsLibrary = require('fs');
+let data = 'hello there';
+fsLibrary.writeFile('../project/MarsMapMaker-1/src/__tests__/testRunner.js', data, (error) => {
+    if (error) throw error;
     })
-<script/>
