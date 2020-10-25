2942eeaee8dec0712cb3f5f019b55da2a6352fb8
hafey1 <niggebruggere@g.cofc.edu>
2020-10-25 19:10:36 -0400

    messing with submodule

--- TestAutomation/scripts/runAllTests.js
+++ TestAutomation/scripts/runAllTests.js
@@ -11,16 +11,47 @@ function resolveAfter2Seconds() {
 }
 
 function testRunnerContent(fromCases) {
+    let propHandle = ""
+
+    
+    
     let imports = "import React from \'react\';\nimport { shallow } from \'enzyme\'\n//{ } imports non default component\n"
     console.log("sdfasffdsfa " + fromCases.length)
     for (let i = 0; i < fromCases.length; i++) {
         let tCase = fromCases[i]
         if (!imports.includes(tCase.module))
-            imports = imports + "import { " + tCase.module + " } from \"../components/" + tCase.module + ";\n"
+            imports = imports + "import { " + tCase.module + " } from \"../components/" + tCase.module + "\";\n"
     }
     imports = imports + "\n"
 
-    return imports;
+    let testCalls = "\n"
+    
+    for(let i = 0; i < fromCases.length; i++){
+        let tCase = fromCases[i]
+        
+        if (tCase.module === "DropDown") {
+            propHandle = "list={[]}"
+        }
+        
+        //first line of function
+        testCalls = testCalls + "describe(\'" + tCase.module + " Component\', () => {\n"
+
+        //second line of function
+        testCalls = testCalls + "\tit(\'" + tCase.metaDataShort + "\', () => {\n"
+
+        let propID = tCase.module
+        //third line of function
+        testCalls = testCalls + "\t\tconst wrapper = shallow(<" + tCase.module + " " + propHandle + " />);\n"
+        
+        //fourth line of funciton
+        testCalls = testCalls + "\t\tlet result = wrapper.instance()." + tCase.functionName + "(" + JSON.stringify(tCase.input) + ");\n"
+
+        //fifth line of function
+        testCalls = testCalls + "\t\texpect(result).toBe(" + tCase.expectedOutput + ");\n\t});\n});\n\n"
+
+    }
+
+    return imports + testCalls;
 }
 
 
@@ -54,6 +85,9 @@ console.log('Finished Processing ' + infoForTestRunner.length + " Test Case(s)."
 //this will write the proper file information soon
 const data = testRunnerContent(infoForTestRunner);
 
+const anotherWait = await resolveAfter2Seconds();
+console.log( "hello" + data)
+console.log("boodbye" + infoForTestRunner)
 //write the info to testRunner
 fsLibrary.writeFile('../project/MarsMapMaker-1/src/__tests__/testRunner.js', data, (error) => {
     
--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -2,5 +2,5 @@ node runAllTests.js
 echo "building tests.."
 sleep 2
 echo "done"
-cd ../project/MarsMapMaker-1/src/__tests__
-node testRunner.js
+cd ../project/MarsMapMaker-1/
+npm test
