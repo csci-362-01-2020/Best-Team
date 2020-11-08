930e45fe6f883452b9c4cdc1f7afa6f416b0dac2
hafey1 <niggebruggere@g.cofc.edu>
2020-11-07 19:55:49 -0500

    removes unnecessary files in each run case

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -8,5 +8,8 @@ cd ../../scripts
 echo "verifying results..."
 node verifyResults.js
 sleep 2
-echo "displaying results to HTML..."
+echo "displaying results to HTML, please wait..."
 xdg-open ../reports/finalReport.HTML
+echo "test results desplaying..."
+sleep 10
+rm ../temp/actualResults.json ../oracles/testOracles.json ../reports/finalReport.HTML
