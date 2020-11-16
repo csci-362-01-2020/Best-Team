d524c6c1015c1cb7c2436f84d985fd95ff3c257f
hafey1 <niggebruggere@g.cofc.edu>
2020-11-16 17:07:20 -0500

    css tinkering

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -12,4 +12,4 @@ echo "displaying results to HTML, please wait..."
 xdg-open ../reports/finalReport.HTML
 echo "test results desplaying..."
 sleep 10
-rm ../temp/actualResults.json ../oracles/testOracles.json ../reports/finalReport.HTML ../project/MarsMapMaker/src/__tests/testRunner.js
+rm ../temp/actualResults.json ../oracles/testOracles.json ../project/MarsMapMaker/src/__tests__/testRunner.js
