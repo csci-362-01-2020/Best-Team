5a783afbcd80f9df2cd1aff3bd7297204cb9ad2e
hafey1 <niggebruggere@g.cofc.edu>
2020-11-22 14:40:22 -0500

    changing quickSetUp

--- TestAutomation/quickSetUp.sh
+++ TestAutomation/quickSetUp.sh
@@ -1,8 +1,7 @@
 echo "initiating start-up"
 cd project/MarsMapMaker/
-git submodule update --init
-git pull https://github.com/hafey1/MarsMapMaker
 echo "grabbing dependencies, please wait..."
 npm install
+npm audit fix
 cd ../../
 echo "good to go, run command: bash runAllTests.sh"
